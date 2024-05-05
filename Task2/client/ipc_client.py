2dbae8ce5c845081d0e9292d37fe3b1ff3c61dd7
MuZhou233 <muzhou233@outlook.com>
2024-05-05 17:11:20 +0100

    update

--- Task2/client/ipc_client.py
+++ Task2/client/ipc_client.py
@@ -3,13 +3,30 @@
 # Base https://medium.com/techanic/docker-containers-ipc-using-sockets-part-1-2ee90885602c
 
 import socket
+import pickle
+import random
 
 HOST = socket.gethostbyname('ipc_server_dns_name')
 PORT = 9898        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world. IPC success!')
+    s.sendall(pickle.dumps({'command': 'ping'}))
     data = s.recv(1024)
+    if data == b'pong':
+        print('Connected to server')
+        # random 50 numbers
+        numbers = []
+        for i in range(50):
+            numbers.append(random.randint(1, 100))
+        print('Sending', numbers)
+        s.sendall(pickle.dumps({
+            'command': 'math',
+            'numbers': numbers,
+        }))
+        data = s.recv(1024)
+        print('Received', repr(pickle.loads(data)))
+        s.sendall(pickle.dumps({'command': 'exit'}))
+    else:
+        print('Failed to connect to server')
 
-print('Received', repr(data))
--- Task2/server/ipc_server.py
+++ Task2/server/ipc_server.py
@@ -3,9 +3,11 @@
 # Base https://medium.com/techanic/docker-containers-ipc-using-sockets-part-1-2ee90885602c
 
 import socket
+import pickle
 
 HOST = socket.gethostbyname('ipc_server_dns_name')
 PORT = 9898  # Port to listen on (non-privileged ports are > 1023)
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -18,4 +20,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
-            conn.sendall(data)
+            data = pickle.loads(data)
+            match data['command']:
+                case 'exit':
+                    break
+                case 'ping':
+                    conn.sendall(b'pong')
+                case 'math':
+                    numbers = data['numbers']
+                    res = {
+                        'mean': sum(numbers) / len(numbers),
+                        'median': sorted(numbers)[len(numbers) // 2],
+                        'standard_deviation': (sum((x - sum(numbers) / len(numbers)) ** 2 for x in numbers) / len(numbers)) ** 0.5
+                    }
+                    conn.sendall(pickle.dumps(res))
+                case _:
+                    conn.sendall(b'Unknown command')
